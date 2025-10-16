62c2b9612c2727852ca44bb3333534d397c92092
kaelkier <rndiaditya.pratama@gmail.com>
2025-10-16 13:08:18 +0700

    feat: hash password & new schema

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
 import cloudinary from '../utils/cloudinary.js';
 import fs from 'fs';
+import bcrypt from 'bcrypt';
 
 const generateToken = (userId) => {
     return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
@@ -9,22 +10,35 @@ const generateToken = (userId) => {
 
 export const registerUser = async (req, res) => {
     try {
-        const { name, email, password } = req.body;
+        const { username, name, email, password } = req.body;
 
-        if (!name || !email || !password) {
+        if (!username || !name || !email || !password) {
             return res.status(400).json({ message: 'All fields are required' });
-        };
+        }
 
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
+        const existingUsername = await User.findOne({ username });
+        if (existingUsername) {
+            return res.status(400).json({ message: 'Username already taken' });
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
             return res.status(400).json({ message: 'User with this email already exists'});
-        };
+        }
 
-        const newUser = await User.create({ name, email, password });
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({ username, name, email, password: hashPassword });
 
         res.status(201).json({
             message: 'Registration successful',
-            user: { id: newUser._id, name: newUser.name, email: newUser.email },
+            user: { 
+                id: newUser._id, 
+                username: newUser.username, 
+                name: newUser.name, 
+                email: newUser.email 
+            },
         });
     } catch (error) {
         res.status(500).json({ message: 'Server error during registration', error: error.message});
@@ -37,12 +51,17 @@ export const loginUser = async (req, res) => {
 
         if(!email || !password) {
             return res.status(400).json({ message: 'Email dan password are required' });
-        };
+        }
 
         const user = await User.findOne({ email });
-        if(!user || user.password !== password) {
+        if(!user) {
             return res.status(401).json({ message: 'Invalid email or password'});
-        };
+        }
+
+        const isMatchPw = await bcrypt.compare(password, user.password)
+        if(!isMatchPw) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
 
         const token = generateToken(user._id);
 
--- models/User.js
+++ models/User.js
@@ -1,11 +1,20 @@
 import mongoose from 'mongoose';
 
 export const userSchema = new mongoose.Schema({
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        trim: true, 
+        lowercase: true, 
+        minlength: 3, 
+        maxlength: 20,
+    },
     name: { type: String, required: true },
     email: { type: String, unique: true ,required: true },
     password: { type: String, required: true },
     avatar: { type: String },
-    cloudinaryPublicId: { type: String }
+    avatarPublicId: { type: String }
 });
 
 export default mongoose.model('User', userSchema);
