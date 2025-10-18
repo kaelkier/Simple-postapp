12a08a2f657c71b0ca24f2d1d9d29bd38f6c610e
kaelkier <rndiaditya.pratama@gmail.com>
2025-10-18 21:27:39 +0700

    refactor: add new field and update structure

--- models/User.js
+++ models/User.js
@@ -1,26 +1,46 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 export const userSchema = new mongoose.Schema({
-    username: { 
-        type: String, 
-        required: true, 
-        unique: true, 
-        trim: true, 
-        lowercase: true, 
-        minlength: 3, 
-        maxlength: 20,
-    },
-    name: { type: String, required: true },
-    email: { type: String, unique: true ,required: true },
-    password: { type: String, required: true },
-    avatar: { 
+    avatar: {
         type: String,
         default: 'https://res.cloudinary.com/dgl06pqcl/image/upload/v1760713012/user_g2rplj.png'
     },
-    avatarPublicId: { 
+    avatarPublicId: {
         type: String,
         default: 'user_g2rplj'
-    }
-});
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        minlength: 3,
+        maxlength: 20,
+        trim: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Please enter a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+        trim: true
+    },
+    bio: { type: String, maxlength: 150 }
+}, { timestamps: true });
 
 export default mongoose.model('User', userSchema);
