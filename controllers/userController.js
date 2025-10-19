9f6554051ed2c48ff0685ab77bc3dc938a724660
kaelkier <rndiaditya.pratama@gmail.com>
2025-10-19 14:35:06 +0700

    fix: select password field in login controller & change status message

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,17 +53,17 @@ export const loginUser = async (req, res) => {
         const { email, password } = req.body;
 
         if(!email || !password) {
-            return res.status(400).json({ message: 'Email dan password are required' });
+            return res.status(400).json({ message: 'Email and password are required' });
         }
 
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select('+password');
         if(!user) {
             return res.status(401).json({ message: 'Invalid email or password'});
         }
 
         const isMatchPw = await bcrypt.compare(password, user.password)
         if(!isMatchPw) {
-            return res.status(401).json({ message: 'Invalid email or password' });
+            return res.status(401).json({ message: 'Invalid password' });
         }
 
         const token = generateToken(user._id);
