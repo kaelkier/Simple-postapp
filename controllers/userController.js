45f080349ca71bb2e9db9778b97c70d95b1c672c
kaelkier <rndiaditya.pratama@gmail.com>
2025-10-22 13:46:36 +0700

    fix: return

--- controllers/userController.js
+++ controllers/userController.js
@@ -172,7 +172,7 @@ export const deleteAvatar = async (req, res) => {
         const user = await User.findById(req.user.id);
 
         if (!user) {
-            res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: 'User not found' });
         }
 
         if (user.avatarPublicId) {
