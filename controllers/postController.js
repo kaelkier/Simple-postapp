7d04a2d4388928fa83ed5bef2cb8a194edfc08e5
kaelkier <rndiaditya.pratama@gmail.com>
2025-10-22 10:29:31 +0700

    fix: correct logic to allow post creation with optional content or image

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,26 +6,34 @@ export const createPost = async (req, res) => {
     try {
         const { content } = req.body;
 
-        if (!content || !req.file) {
-            return res.status(400).json({ message: 'Title and content are required'});
-        };
+        let imageUrl = null;
+        let publicId = null;
 
-        const result = await cloudinary.uploader.upload(req.file.path, {
-            folder: "image_posts",
-        });
+        if (!content && !req.file) {
+            return res.status(400).json({ message: 'Post must contain content or image'});
+        }
+        
+        if (req.file) {
+            const result = await cloudinary.uploader.upload(req.file.path, {
+                folder: 'image_posts',
+            });
 
-        if (fs.existsSync(req.file.path)) {
-            fs.unlinkSync(req.file.path);
+            if (fs.existsSync(req.file.path)) {
+                fs.unlinkSync(req.file.path);
+            }
+
+            imageUrl = result.secure_url;
+            publicId = result.public_id;
         }
         
         const post = await Post.create({
             content,
-            image: result.secure_url,
-            imagePublicId: result.public_id,
+            image: imageUrl,
+            imagePublicId: publicId,
             user: req.user.id,
         });
 
-        res.status(200).json({ message: 'Post successfully created', post });
+        res.status(201).json({ message: 'Post successfully created', post });
     } catch (error) {
         res.status(500).json({ message: 'Failed to create post', error: error.message });
     }
